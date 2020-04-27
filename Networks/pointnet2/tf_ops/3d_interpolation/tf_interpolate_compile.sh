40da808c3f204428c379e4b45c0f0a88ae35f66d
Tiancheng <txu17@ur.rochester.edu>
2020-04-26 23:08:27 -0400

    utils.

---
+++ Networks/pointnet2/tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -0,0 +1,5 @@
+# TF1.4
+CUDA_PATH=/usr/local/cuda-10.2
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -fPIC -I $TF_INC -I $CUDA_PATH/include -lcudart -L $CUDA_PATH/lib64/ -L$TF_LIB -I$TF_INC/external/nsync/public -ltensorflow_framework  -O2 #-D_GLIBCXX_USE_CXX11_ABI=0
---
+++ Networks/pointnet2/tf_ops/sampling/tf_sampling_compile.sh
@@ -0,0 +1,6 @@
+#/bin/bash
+CUDA_PATH=/usr/local/cuda-10.2
+$CUDA_PATH/bin/nvcc tf_sampling_g.cu -o tf_sampling_g.cu.o -c -O2 -DGOOGLE_CUDA=1 -x cu -Xcompiler -fPIC
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
+g++ -std=c++11 tf_sampling.cpp tf_sampling_g.cu.o -o tf_sampling_so.so -shared -fPIC -I $TF_INC -I $CUDA_PATH/include -lcudart -L $CUDA_PATH/lib64/ -L$TF_LIB -I$TF_INC/external/nsync/public -ltensorflow_framework  -O2 #-D_GLIBCXX_USE_CXX11_ABI=0
