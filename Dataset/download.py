66f17f093228168d317d95399a4838e847eb03e9
btb5b <546600519@qq.com>
2020-07-12 03:50:23 -0400

    add harness script and directory for datasets

--- Dataset/download.py
+++ Dataset/download.py
@@ -1,5 +1,14 @@
+'''
+    This script is used to download and unzip datasets that shared by multiple networks.
+'''
+
 import os
 import sys
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--name', type=str, default=None, help='Dataset named, use: --name ModelNet, --name ShapeNet')
+FLAGS = parser.parse_args()
 
 dataset = {}
 
@@ -9,10 +18,8 @@ dataset['ModelNet'] = ['modelnet40_ply_hdf5_2048', 'https://shapenet.cs.stanford
 # ShapeNet
 dataset['ShapeNet'] = ['shapenetcore_partanno_segmentation_benchmark_v0_normal', 'https://shapenet.cs.stanford.edu/media/shapenetcore_partanno_segmentation_benchmark_v0_normal.zip']
 
-name = sys.argv[1]
-
-if not os.path.exists(os.path.join(dataset[name][0])):
-    www = dataset[name][1]
+if not os.path.exists(os.path.join(dataset[FLAGS.name][0])):
+    www = dataset[FLAGS.name][1]
     zipfile = os.path.basename(www)
     os.system("wget %s --no-check-certificate; unzip %s" %(www, zipfile))
     os.system("rm %s" %(zipfile))
