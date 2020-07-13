6236a1a451b77270cf17e0f3b96705efc6847d1b
Boyuan-Tian <546600519@qq.com>
2020-07-13 02:31:36 -0400

    cleanup models/pointnet2_cls_ssg.py

--- Networks/pointnet2/models/pointnet2_cls_ssg.py
+++ Networks/pointnet2/models/pointnet2_cls_ssg.py
@@ -1,19 +1,16 @@
 """
     PointNet++ Model for point clouds classification
 """
-from __future__ import print_function
 
 import os
 import sys
 BASE_DIR = os.path.dirname(__file__)
 sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR, '../utils'))
-
 import tensorflow as tf
 import numpy as np
 import tf_util
 from pointnet_util import pointnet_sa_module
-
 
 def placeholder_inputs(batch_size, num_point):
     pointclouds_pl = tf.placeholder(tf.float32, shape=(batch_size, num_point, 3))
@@ -32,18 +29,10 @@ def get_model(point_cloud, is_training, bn_decay=None):
     # Set abstraction layers
     # Note: When using NCHW for layer 2, we see increased GPU memory usage (in TF1.4).
     # So we only use NCHW for layer 1 until this issue can be resolved.
-    
-    print("module 1:")
     l1_xyz, l1_points, l1_indices = pointnet_sa_module(l0_xyz, l0_points, npoint=512, radius=0.2, nsample=32, mlp=[64,64,128], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer1', use_nchw=True)
-    print("module 1 ends\n")
-    
-    print("module 2:")
     l2_xyz, l2_points, l2_indices = pointnet_sa_module(l1_xyz, l1_points, npoint=128, radius=0.4, nsample=64, mlp=[128,128,256], mlp2=None, group_all=False, is_training=is_training, bn_decay=bn_decay, scope='layer2')
-    print("module 2 ends\n")
-
-    print("module 3:")
     l3_xyz, l3_points, l3_indices = pointnet_sa_module(l2_xyz, l2_points, npoint=None, radius=None, nsample=None, mlp=[256,512,1024], mlp2=None, group_all=True, is_training=is_training, bn_decay=bn_decay, scope='layer3')
-    print("module 3 ends\n")
+
     # Fully connected layers
     net = tf.reshape(l3_points, [batch_size, -1])
     net = tf_util.fully_connected(net, 512, bn=True, is_training=is_training, scope='fc1', bn_decay=bn_decay)
@@ -64,11 +53,9 @@ def get_loss(pred, label, end_points):
     tf.add_to_collection('losses', classify_loss)
     return classify_loss
 
-def print(args):
-    return
 
 if __name__=='__main__':
     with tf.Graph().as_default():
-        inputs = tf.zeros((8,1024,3))
+        inputs = tf.zeros((32,1024,3))
         output, _ = get_model(inputs, tf.constant(True))
         print(output)
